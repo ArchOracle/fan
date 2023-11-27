847396dbb3147d053b413e7cb15acf738cebca7e
ArchOracle <wowakk98@gmail.com>
2023-11-27 18:18:18 +0300

    Библиотека картинок: написал метод заполнения матриц изображения.

--- src/libs/js/image/image.js
+++ src/libs/js/image/image.js
@@ -14,4 +14,20 @@ export class ImagePixels {
 		this.width = width
 		this.imageData = imageData
 	}
+
+	fillMatrixes() {
+		this.redMatrix = new Matrix(this.height, this.width, Uint8ClampedArray)
+		this.greenMatrix = new Matrix(this.height, this.width, Uint8ClampedArray)
+		this.blueMatrix = new Matrix(this.height, this.width, Uint8ClampedArray)
+		this.alphaMatrix = new Matrix(this.height, this.width, Uint8ClampedArray)
+
+		for (let y = 0; y < this.height; y += 1) {
+			for (let x = 0; x < this.width; x += 1) {
+				this.redMatrix.set(x, y, this.imageData.data[y * this.width * 4 + x * 4])
+				this.greenMatrix.set(x, y, this.imageData.data[y * this.width * 4 + x * 4 + 1])
+				this.blueMatrix.set(x, y, this.imageData.data[y * this.width * 4 + x * 4 + 2])
+				this.alphaMatrix.set(x, y, this.imageData.data[y * this.width * 4 + x * 4 + 3])
+			}
+		}
+	}
 }
