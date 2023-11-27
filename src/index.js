2f06fba6adb7e3940c8a69a9f610b7ea58acc11b
ArchOracle <wowakk98@gmail.com>
2023-11-27 23:42:46 +0300

    Написал функцию для тестового взаимодействия с канвасом.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
 import {ImagePixels} from "./libs/js/image/image";
+
+function createTestImage(canvasSelector) {
+	let canvas = document.querySelector(canvasSelector)
+	let context = canvas.getContext('2d')
+
+	let imagePixel = new ImagePixels(context.createImageData(canvas.height, canvas.width))
+
+	for (let x = 0; x < imagePixel.width; x += 1) {
+		for (let y = 0; y < imagePixel.height; y +=1 ) {
+			imagePixel.alphaMatrix.set(x, y, 255)
+			if (Math.random() < 0.05) {
+				imagePixel.redMatrix.set(x, y, 255)
+			}
+			if (Math.random() < 0.05) {
+				imagePixel.greenMatrix.set(x, y, 255)
+			}
+			if (Math.random() < 0.05) {
+				imagePixel.blueMatrix.set(x, y, 255)
+			}
+		}
+	}
+	imagePixel.fillImageData(context)
+}
