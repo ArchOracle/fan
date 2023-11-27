4f9219c0e11bd1a42a022330a6f4ce4e56925e94
ArchOracle <wowakk98@gmail.com>
2023-11-27 23:41:40 +0300

    Библиотека карты: заложил основы, написал конструктор.

--- src/libs/js/map/map.js
+++ src/libs/js/map/map.js
@@ -1,3 +1,17 @@
-export class Map {
+import {Matrix} from "../matrix/matrix";
 
+export class Map {
+	storage
+	height
+	width
+
+	constructor(height, width) {
+		this.height = height
+		this.width = width
+		this.storage = new Matrix(height, width, Array)
+	}
+
+	evaluate() {
+
+	}
 }
