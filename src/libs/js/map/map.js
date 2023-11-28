979a4199adc16f6881dd368526cdb4bc909d5d39
ArchOracle <wowakk98@gmail.com>
2023-11-28 10:30:20 +0300

    Библиотека карты: написал метод эволюции состояния на один шаг.

--- src/libs/js/map/map.js
+++ src/libs/js/map/map.js
@@ -12,6 +12,13 @@ export class Map {
 	}
 
 	evaluate() {
-
+		let nextState = new Matrix(this.height, this.width, Array)
+		for (let y = 0; y < this.height; y += 1) {
+			for (let x = 0; x < this.width; x += 1) {
+				this.storage.get(x, y).forEach((agent) => {
+					agent.evaluate(this.storage, nextState)
+				})
+			}
+		}
 	}
 }
