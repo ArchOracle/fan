4e0bdee1b703c7c3ab45eed23b8168e8e0ac5416
ArchOracle <wowakk98@gmail.com>
2023-11-28 18:55:39 +0300

    Написал функцию для тестового засева карты.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import {ImagePixels} from "./libs/js/image/image";
 import {Map} from "./libs/js/map/map";
+import {SimpleAgent} from "./simple_charge/agents";
 
 if (window.location.pathname === "/simple_charge/index.html") {
 	document.addEventListener('DOMContentLoaded', () => {
@@ -28,4 +29,16 @@ function createTestImage(canvasSelector) {
 		}
 	}
 	imagePixel.fillImageData(context)
+}
+
+function seedTestMap() {
+	let map = new Map(600, 600)
+	for (let x = 0; x < map.width; x += 1) {
+		for (let y = 0; y < map.height; y +=1 ) {
+			if (Math.random() < 0.05) {
+				map.addAgent(new SimpleAgent(x, y))
+			}
+		}
+	}
+	return map
 }
