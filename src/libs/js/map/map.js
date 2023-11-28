c1f9e62acb68c4623e21cca6ed03fba042cb4a8b
ArchOracle <wowakk98@gmail.com>
2023-11-28 19:21:35 +0300

    Библиотека карты: сделал проверку на наличие списка агентов перед эволюцией карты.

--- src/libs/js/map/map.js
+++ src/libs/js/map/map.js
@@ -15,9 +15,12 @@ export class Map {
 		let nextState = new Matrix(this.height, this.width, Array)
 		for (let y = 0; y < this.height; y += 1) {
 			for (let x = 0; x < this.width; x += 1) {
-				this.storage.get(x, y).forEach((agent) => {
-					agent.evaluate(this.storage, nextState)
-				})
+				let agentList = this.storage.get(x, y)
+				if (Array.isArray(agentList)) {
+					agentList.forEach((agent) => {
+						agent.evaluate(this.storage, nextState)
+					})
+				}
 			}
 		}
 		this.storage = nextState
