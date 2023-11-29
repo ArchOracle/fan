d0516ce62ef8b7bc7a2947cd3cb88538686506df
ArchOracle <wowakk98@gmail.com>
2023-11-29 12:34:41 +0300

    Библиотека карты: сделал сохранение данных агента частью родительского конструктора.

--- src/libs/js/map/agent.js
+++ src/libs/js/map/agent.js
@@ -3,9 +3,11 @@ import {Matrix} from "../matrix/matrix";
 export class Agent {
 	x
 	y
+	agentData
 	constructor(x, y, agentData) {
 		this.x = x
 		this.y = y
+		this.agentData = agentData
 	}
 
 	evaluate(thisState, nextState) {
