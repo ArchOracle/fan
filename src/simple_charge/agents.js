82a48ed6e89d0a2e408715c0335dfca2e8e9dcc6
ArchOracle <wowakk98@gmail.com>
2023-11-30 18:06:33 +0300

    Простой агент: сделал движение в центр.

--- src/simple_charge/agents.js
+++ src/simple_charge/agents.js
@@ -8,8 +8,18 @@ export class SimpleAgent extends Agent{
 	}
 	evaluate(thisState, nextState) {
 		super.evaluate(thisState, nextState)
-		this.x += (300 - this.x) / 10
-		this.y += (300 - this.y) / 10
-		Map.addAgentToStorage(this, nextState)
+		const ox = thisState.width / 2
+		const oy = thisState.height / 2
+		const xx = (this.agentData.x - ox)
+		const yy = (this.agentData.y - oy)
+		const s = (xx ** 2 + yy ** 2) ** 0.5
+		const v = s / ((ox ** 2 + oy ** 2) ** 0.5)
+		const dx = -1 * (v * xx / 20)
+		const dy = -1 * (v * yy / 20)
+		const newX = ox + xx + dx
+		const newY = oy + yy + dy
+		if (newX > 1 && newX < 599 && newY > 1 && newY < 599) {
+			Map.addAgentToStorage(new SimpleAgent(Math.round(newX), Math.round(newY), {x: newX, y: newY}), nextState)
+		}
 	}
 }
