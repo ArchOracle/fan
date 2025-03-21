b5ab00b366b3254548a09d24c4523d6daf5544a3
ArchOracle <wowakk98@gmail.com>
2025-03-22 00:51:39 +0300

    Математика: заложил класс для проверки наличия и поиска точек пересечений чего угодно с чем угодно, добавил метод поиска пересечений отрезков.

---
+++ src/map/lib/tools/math/intersect.ts
@@ -0,0 +1,44 @@
+import {Vector} from "./vector";
+import {Line} from "./primitive/line";
+
+export class Intersect {
+    public intersectPointList: Array<Vector> = []
+
+    checkIntersectLineWithLine(from: Line, to: Line) {
+        let fromStartX: number = from.start.x
+        let fromStartY: number = from.start.y
+        let fromEndX: number = from.end.x
+        let fromEndY: number = from.end.y
+
+        let toStartX: number = to.start.x
+        let toStartY: number = to.start.y
+        let toEndX: number = to.end.x
+        let toEndY: number = to.end.y
+
+        let Ax = (toStartX - fromStartX) / (fromEndX - fromStartX)
+        let Bx = (toEndX - toStartX) / (fromEndX - fromStartX)
+
+        let Ay = (toStartY - fromStartY) / (fromEndY - fromStartY)
+        let By = (toEndY - toStartY) / (fromEndY - fromStartY)
+
+        let t2 = (Ay - Ax) / (Bx - By)
+
+        let t1x = Ax + Bx * t2
+        let t1y = Ay + By * t2
+
+        if (
+            Math.abs(t1x - t1y) < 1e-6 &&
+            t1x > 1e-3 && t1x < 1 - 1e-3 &&
+            t2 > 1e-3 && t2 < 1 - 1e-3
+        ) {
+            this.intersectPointList.push(
+                new Vector(
+                    fromStartX + t1x * (fromEndX - fromStartX),
+                    fromStartY + t1y * (fromEndY - fromStartY),
+                )
+            )
+        }
+
+        return this
+    }
+}
