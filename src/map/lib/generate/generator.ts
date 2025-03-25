c187fac356e79fdb5fc53ff3f19ff574ca447574
ArchOracle <wowakk98@gmail.com>
2025-03-25 19:56:15 +0300

    Генератор: сделал метод объединения кругов, сделал метод удаления связей и точек, сделал чтобы при фиксе генерации удалялись точки и связи, а не создавались.

--- src/map/lib/generate/generator.ts
+++ src/map/lib/generate/generator.ts
@@ -3,6 +3,7 @@ import {Link} from "../location/link";
 import {Vector} from "../tools/math/vector";
 import {Intersect} from "../tools/math/intersect";
 import {Line} from "../tools/math/primitive/line";
+import {Circle} from "../tools/math/primitive/circle";
 
 export class Generator {
     private readonly context: CanvasRenderingContext2D;
@@ -10,7 +11,7 @@ export class Generator {
     protected linkList: Array<Link> = []
     protected evaluateProcessed: boolean = false
     protected fixGenerationCount = 0
-    protected fixGenerationMax = 2
+    protected fixGenerationMax = 55
 
     constructor(public pointCount: number = 10, public linkCount: number = 20, protected canvas: HTMLCanvasElement|null) {
         if (canvas === null) {
@@ -36,10 +37,10 @@ export class Generator {
         for (let i = 0; i < this.pointCount; i++) {
             this.pointList.push(
                 Point.create(
-                    new Vector(600 + (Math.random()-0.5) * 200, 450 + (Math.random()-0.5) * 200),
+                    new Vector(750 + (Math.random()-0.5) * 375, 500 + (Math.random()-0.5) * 250),
                     new Vector(),
                     new Vector()
-                ).setSize(5 * Math.round(Math.random() * 3) + 5)
+                ).setSize(Point.getRandomSize())
             )
         }
         for (let i = 0; i < this.linkCount; i++) {
@@ -52,7 +53,7 @@ export class Generator {
                 (new Link(
                     this.pointList[indexPointStart],
                     this.pointList[indexPointEnd]
-                )).setSize(Math.round(Math.random() * 300) + 50)
+                )).setSize(Link.getRandomSize())
             )
         }
 
@@ -110,11 +111,9 @@ export class Generator {
             setTimeout(() => {
                 this.draw()
                 this.evaluate()
-            }, 10)
+            }, 5)
         } else {
-            setTimeout(() => {
-                this.fixGenerate()
-            }, 1000)
+            this.fixGenerate()
             this.draw()
             this.evaluateProcessed = false;
             (<HTMLElement>document.getElementById('completed')).style.display = 'block'
@@ -144,7 +143,16 @@ export class Generator {
     }
 
     fixIntersect() {
-        return this.fixIntersectLink()
+        // return this.fixIntersectCircle()
+        let isIL = this.fixIntersectLink()
+        let isPL = this.fixIntersectCircle()
+
+        this.pointList.forEach((point) => {
+            if (point.getLinkList().length < 3) {
+                this.addRandomLinkForPoint(point)
+            }
+        })
+        return isIL || isPL
     }
 
     fixIntersectLink() {
@@ -167,13 +175,85 @@ export class Generator {
         }
 
         intersectPairList.forEach((intersectPair) => {
-            this.splitIntersectLines(intersectPair)
+            // this.splitIntersectLines(intersectPair)
+            this.removeIntersectLinks(intersectPair)
         })
 
         this.clearEqualLink()
 
         // alert(intersectPairList.length)
+        // this.draw()
         return intersectPairList.length > 0
+    }
+
+    fixIntersectCircle() {
+        let intersectPairList: Array<IntersectCirclePair> = []
+        for (let i = 0; i < this.pointList.length; i += 1) {
+            for (let j = i + 1; j < this.pointList.length; j++) {
+                let from = this.pointList[i], to = this.pointList[j]
+                let intersectCircleList = (new Intersect()).checkIntersectCircleWithCircle(
+                    new Circle(from.position, from.getSize()),
+                    new Circle(to.position, to.getSize()),
+                ).intersectCircleList
+                if (intersectCircleList.length > 0) {
+                    intersectPairList.push({
+                        first: from,
+                        second: to
+                    })
+                }
+            }
+        }
+
+        intersectPairList.forEach((intersectPair) => {
+            //this.unionIntersectCircles(intersectPair)
+            this.removeIntersectPoints(intersectPair)
+        })
+        // this.draw()
+
+        return intersectPairList.length > 0
+    }
+
+    removeIntersectLinks(intersectPair: IntersectLinePair) {
+        let linksForFirstLink = intersectPair.first.getPointStart().getLinkList().length +
+            intersectPair.first.getPointEnd().getLinkList().length
+        let linksForSecondLink = intersectPair.second.getPointStart().getLinkList().length +
+            intersectPair.second.getPointEnd().getLinkList().length
+        if (linksForFirstLink < linksForSecondLink) {
+            this.removeLinkList([intersectPair.first])
+        } else {
+            this.removeLinkList([intersectPair.second])
+        }
+    }
+
+    removeIntersectPoints(intersectPair: IntersectCirclePair) {
+        let removedPoint: Point, livePoint: Point
+        if (intersectPair.first.getLinkList().length < intersectPair.second.getLinkList().length) {
+            removedPoint = intersectPair.first
+            livePoint = intersectPair.second
+        } else {
+            removedPoint = intersectPair.second
+            livePoint = intersectPair.first
+        }
+        for (let j = 0; j < removedPoint.getLinkList().length; j += 1) {
+            this.addRandomLinkForPoint(livePoint)
+        }
+        this.removePoint(removedPoint)
+        this.pointList.forEach((point) => {
+            if (point.getLinkList().length < 2) {
+                this.addRandomLinkForPoint(point)
+            }
+        })
+    }
+
+    addRandomLinkForPoint(point: Point) {
+        let i = Math.round(Math.random() * (this.pointList.length - 1))
+        i = i < (this.pointList.length - 1) ? i : i - 1
+        let newPoint = this.pointList[i]
+        newPoint = newPoint === point ? this.pointList[i + 1] : newPoint
+        let newLink = (new Link(point, newPoint)).setSize(Link.getRandomSize())
+        this.linkList.push(newLink)
+        point.addLink(newLink)
+        newPoint.addLink(newLink)
     }
 
     splitIntersectLines(intersectPair: IntersectLinePair) {
@@ -189,28 +269,74 @@ export class Generator {
 
         this.pointList.push(intersectPoint)
 
-        let sizeMultiplier = -0.5
+        let sizeMultiplier = Math.random()
 
+        let sizeNewLink = (intersectPoint.getSize() * 0 + intersectPair.first.getCurrentSize()) * sizeMultiplier
+        let minSizeLink = 100
+        sizeNewLink = sizeNewLink > minSizeLink ? sizeNewLink : minSizeLink
         this.linkList.push(
             (new Link(firstStart, intersectPoint))
-                .setSize((intersectPoint.getSize() * 0 - intersectPair.first.getCurrentSize()) * sizeMultiplier)
+                .setSize(sizeNewLink)
         )
+        sizeNewLink = (intersectPoint.getSize() * 0 + intersectPair.first.getCurrentSize()) * (1- sizeMultiplier)
+        sizeNewLink = sizeNewLink > minSizeLink ? sizeNewLink : minSizeLink
         this.linkList.push(
             (new Link(firstEnd, intersectPoint))
-                .setSize((intersectPoint.getSize() * 0 - intersectPair.first.getCurrentSize()) * sizeMultiplier)
+                .setSize(sizeNewLink)
         )
+        sizeNewLink = (intersectPoint.getSize() * 0 + intersectPair.second.getCurrentSize()) * sizeMultiplier
+        sizeNewLink = sizeNewLink > minSizeLink ? sizeNewLink : minSizeLink
         this.linkList.push(
             (new Link(secondStart, intersectPoint))
-                .setSize((intersectPoint.getSize() * 0 - intersectPair.second.getCurrentSize()) * sizeMultiplier)
+                .setSize(sizeNewLink)
         )
+        sizeNewLink = (intersectPoint.getSize() *0 + intersectPair.second.getCurrentSize()) * (1 - sizeMultiplier)
+        sizeNewLink = sizeNewLink > minSizeLink ? sizeNewLink : minSizeLink
         this.linkList.push(
             (new Link(secondEnd, intersectPoint))
-                .setSize((intersectPoint.getSize() *0 - intersectPair.second.getCurrentSize()) * sizeMultiplier)
+                .setSize(sizeNewLink)
         )
 
         this.linkList = this.linkList.filter((link) => {
             return link != intersectPair.first.remove() && link != intersectPair.second.remove()
         })
+    }
+
+    unionIntersectCircles(intersectCircles: IntersectCirclePair) {
+        let from: Circle = new Circle(intersectCircles.first.position, intersectCircles.first.getSize()),
+            to: Circle = new Circle(intersectCircles.second.position, intersectCircles.second.getSize())
+
+        let newCenter = from.center.copy().add(to.center.copy()).multipleOnScalar(0.5),
+            newSize = from.size + to.size
+
+        let newPoint = Point.create(newCenter, new Vector(), new Vector()).setSize(newSize/2)
+
+        intersectCircles.first.getLinkList().forEach((link) => {
+            this.changePointInLink(link, intersectCircles.first, newPoint)
+        })
+
+        intersectCircles.second.getLinkList().forEach((link) => {
+            this.changePointInLink(link, intersectCircles.second, newPoint)
+        })
+
+        this.removePoint(intersectCircles.first)
+        this.removePoint(intersectCircles.second)
+
+        this.pointList.push(newPoint)
+
+        this.clearEqualLink()
+
+    }
+
+    changePointInLink(link: Link, oldPoint: Point, newPoint: Point) {
+        if (link.getPointStart() === oldPoint) {
+            link.setPointStart(newPoint)
+            oldPoint.removeLink(link)
+        }
+        if (link.getPointEnd() === oldPoint) {
+            link.setPointEnd(newPoint)
+            oldPoint.removeLink(link)
+        }
     }
 
     clearEqualLink() {
@@ -232,6 +358,21 @@ export class Generator {
         })
     }
 
+    removePoint(removingPoint: Point) {
+        this.removeLinkList(removingPoint.getLinkList())
+        this.pointList = this.pointList.filter((point) => {
+            return point != removingPoint
+        })
+    }
+
+    removeLinkList(removingLinkList: Array<Link>) {
+        removingLinkList.forEach((removingLink) => {
+            this.linkList = this.linkList.filter((link) => {
+                return link != removingLink.remove()
+            })
+        })
+    }
+
     draw() {
         // alert('Высота: ' + this.canvas?.height + '; ширина : ' + this.canvas?.width)
         this.context.clearRect(0, 0, <number>this.canvas?.width, <number>this.canvas?.height)
@@ -245,3 +386,4 @@ export class Generator {
 }
 
 export type IntersectLinePair = {first: Link, second: Link, point: Vector}
+export type IntersectCirclePair = {first: Point, second: Point}
